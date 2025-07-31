73573dcb84ee4652d4f88abd759e686469bf9a5b
nathanoc <72704080+nathanoc@users.noreply.github.com>
2025-07-31 23:58:45 +0100

    Quartz sync: Jul 31, 2025, 11:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Nathan O'Connor",
+    pageTitleSuffix: "Personal Website",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "nathanoc.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
